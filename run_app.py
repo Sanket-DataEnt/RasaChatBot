e123adc6e8ed6c4bd2d9b382bd07ba1cf43a77f2
Sanket Maheshwari <sanketmaheshwari96@gmail.com>
2019-03-05 00:27:41 +0530

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -7,9 +7,9 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/current')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-3787543512-350934098721-539256742071-7724937e1d8546e6848b18d164f7ce09', #app verification token
-							'xoxb-3787543512-539614799910-QTsNHySAzoEzm8PW1QNd1te4', # bot verification token
-							'pgr4D9oUouYTEPL74l3vZGAW', # slack verification token
+input_channel = SlackInput('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', #app verification token
+							'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', # bot verification token
+							'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', # slack verification token
 							True) #bug mode True
 
 agent.handle_channel(HttpInputChannel(5004, '/', input_channel))
